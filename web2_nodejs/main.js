8a2d5934dc8bcc0908d18c5b088a74fc02cb43de
lopahn2 <lopahn2@gmail.com>
2022-03-11 14:21:38 +0000

    2022.03.11

--- web2_nodejs/main.js
+++ web2_nodejs/main.js
@@ -5,7 +5,7 @@ var qs = require('querystring');
 
 
 
-function templateHTML(title, list, body) {
+function templateHTML(title, list, body, control) {
 	return `
 			<!doctype html>
 			<html>
@@ -16,8 +16,8 @@ function templateHTML(title, list, body) {
 			<body>
 			  <h1><a href="/">WEB</a></h1>
 			  ${list}
+			  ${control}
 			  ${body}
-			  <a href='/create'>create</a>
 			</body>
 			</html>
 	
@@ -53,7 +53,7 @@ var app = http.createServer(function(request, response) {
 				
 				
 				var list = templateList(fileList);
-				var template = templateHTML(title, list, `<h2>${title}</h2><p>${description}</p>`);
+				var template = templateHTML(title, list, `<h2>${title}</h2><p>${description}</p>`, `<a href='/create'>create</a>`);
 				
 				response.writeHead(200);
 				response.end(template);
@@ -68,7 +68,14 @@ var app = http.createServer(function(request, response) {
 					
 					title = queryData.id;
 					var list = templateList(fileList);
-					var template = templateHTML(title, list, `<h2>${title}</h2><p>${description}</p>`);
+					var template = templateHTML(title, list, `<h2>${title}</h2><p>${description}</p>`, 
+												`<a href='/create'>create</a> 
+												<a href='/update?id=${title}'>update</a>
+												<form action = 'delete_process' method = 'post' onsubmit='return confirm("정말 삭제하시겠습니까?")'>
+													<input type='hidden' name='id' value='${title}'>
+													<input type='submit' value='delete'>
+												</form>
+												`);
 					
 				response.writeHead(200);
 				response.end(template);
@@ -104,7 +111,40 @@ var app = http.createServer(function(request, response) {
 				response.end(template);
 				
 			});
-	} else if (pathName === '/create_process') {
+	} else if (pathName === '/update') {
+		// read
+		fs.readdir('./data', (err, fileList) => {
+				
+			fs.readFile(`data/${title}`, 'utf8', function(err, description){
+
+				title = queryData.id;
+				var list = templateList(fileList);
+				var template = templateHTML(title, list, `
+				<form action = 'https://hwany.run.goorm.io/update_process' method = 'post'>
+					<input type='hidden' name='id' value='${title}'> 
+					<p>
+						<input type='text' name = 'title' placeholder = 'title' value ='${title}'>
+					</p>
+					<p>
+						<textarea name = 'description' placeholder = 'description'>${description}</textarea>
+					</p>
+					<p>
+						<input type='submit'>
+						<!-- 서버에 데이터를 전송할 때 사용한다. -->
+					</p>
+				</form>
+				
+				`, `<a href='/create'>create</a> <a href='/update?id=${title}'>update</a>`);
+
+			response.writeHead(200);
+			response.end(template);
+			});	
+		});
+		
+		// form
+		
+		
+	}else if (pathName === '/create_process') {
 		var body = '';
 		request.on('data', (data) => {
 			body += data;
@@ -122,7 +162,40 @@ var app = http.createServer(function(request, response) {
 		//폼 데이터를 받아오는 법
 		
 		
-	}else {
+	} else if (pathName === '/update_process') {
+		var body = '';
+		request.on('data', (data) => {
+			body += data;
+		});
+		
+		request.on('end', () => {
+			var post = qs.parse(body);
+			var id = post.id;
+			var title = post.title;
+			var description = post.description;
+			
+			fs.rename(`data/${id}`, `data/${title}`, (err) => {
+				fs.writeFile(`data/${title}`, description, 'utf8', (err1) => {
+					response.writeHead(302, {Location: `/?id=${title}`});
+					response.end();
+				});
+			});
+		});
+	} else if (pathName === '/delete_process') {
+		var body = '';
+		request.on('data', (data) => {
+			body += data;
+		});
+		
+		request.on('end', () => {
+			var post = qs.parse(body);
+			var id = post.id;
+			fs.unlink(`data/${id}`, (err) => {
+				response.writeHead(302, {Location: `/`});
+				response.end();
+			});
+		});
+	} else {
 		response.writeHead(404);
 		response.end('notFound');
 	}
