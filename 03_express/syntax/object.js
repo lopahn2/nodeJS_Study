16959eeef52bdfb813b9421a45f7b704dda50cdd
반영환 <76484900+lopahn2@users.noreply.github.com>
2022-04-10 22:23:48 +0900

    Add files via upload

---
+++ 03_express/syntax/object.js
@@ -0,0 +1,22 @@
+/*
+var members = ['egoing', 'k8805', 'hoya'];
+console.log(members[1]);
+
+var i = 0;
+while(i < members.length) {
+  console.log('array loop', members[i]);
+  i = i + 1;
+}
+*/
+
+var roles = {
+  'programmer':'egoing',
+  'designer':'k8805',
+  'manager':'hoya'
+}
+console.log(roles.designer);
+console.log(roles['designer']);
+
+for(var name in roles) {
+  console.log('object => ', name, 'value => ', roles[name]);
+}
