3cd57dbb74a1ee60609c32b6e73d4c66f1884fd7
lopahn2 <lopahn2@gmail.com>
2022-04-15 14:48:44 +0000

    2022-04-15

---
+++ blog/main.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static(__dirname + "/css"));
+app.use(express.static(__dirname + "/img"));
+
+app.get('/', (req, res) => {
+	res.sendFile(__dirname + "/html/index.html");
+});
+
+app.listen(80);
+
+
+/*
+const express = require("express");
+const server = express();
+
+server.use(express.static(__dirname + "/public"));
+server.use(express.static(__dirname + "/script"));
+
+server.get("/", (req, res) => {
+ 
+  res.sendFile(__dirname + "/html/index.html");
+});
+
+server.get("/about", (req, res) => {
+	res.sendFile(__dirname +"/html/about.html");
+});
+
+/*
+server.use((req, res) => {
+  res.sendFile(__dirname + "/html/error.html");
+});
+*/
+/*
+server.listen(80, (err) => {
+  if (err) return console.log(err);
+  
+});
+
+*/
