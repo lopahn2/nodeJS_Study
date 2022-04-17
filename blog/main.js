22ed05cdeb9dcd39f7cc670fe2c7c25d9217f8b5
lopahn2 <lopahn2@gmail.com>
2022-04-17 13:24:21 +0000

    123

--- blog/main.js
+++ blog/main.js
@@ -1,8 +1,24 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
 
+const mysql = require('mysql');
+const con = mysql.createConnection({
+	host : 'localhost',
+	user : 'root',
+	password : '1q2w3e4r'
+});
+con.connect((err) => {
+	if (err) throw err;
+	console.log("connected!")
+});
+
+app.use(bodyParser.urlencoded({ extended : false }));
 app.use(express.static(__dirname + "/css"));
 app.use(express.static(__dirname + "/img"));
+
+
 
 app.get('/', (req, res) => {
 	res.sendFile(__dirname + "/html/index.html");
@@ -12,6 +28,15 @@ app.get('/signup', (req, res) => {
 	res.sendFile(__dirname + "/html/signUp.html");
 });
 
+app.post('/signup_process', (req,res) => {
+	let post = req.body;
+	let fname = post.fname;
+	let lname = post.lname;
+	let email = post.email;
+	let password = post.password;
+	
+	res.send(post);
+});
 app.listen(80);
 
 
