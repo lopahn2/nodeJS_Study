716ea47093f1c6bdf7f34c7a085549bde75fa87d
lopahn2 <lopahn2@gmail.com>
2022-04-19 14:41:48 +0000

    2022-04-19

--- blog/main.js
+++ blog/main.js
@@ -4,7 +4,15 @@ const crypto = require('crypto');
 
 const app = express();
 
+const session = require('express-session');
+const FileStore = require('session-file-store')(session);
 
+app.use(session({
+	secret : 'hwany wanna go home',
+	resave: false,
+	saveUninitialized : true,
+	store : FileStore()
+}));
 
 const mysql = require('mysql');
 const db = mysql.createConnection({
@@ -28,12 +36,31 @@ app.use(express.static(__dirname + "/img"));
 
 
 app.get('/', (req, res) => {
-	res.sendFile(__dirname + "/html/index.html");
+	console.log(req.session.isLogin);
+	if (req.session.isLogin === true) {
+		res.redirect('/home');
+	} else {
+		res.sendFile(__dirname + "/html/index.html");	
+	}
+	
 });
 
 app.get('/signup', (req, res) => {
 	res.sendFile(__dirname + "/html/signUp.html");
 });
+
+app.get('/home', (req, res) => {
+	if (req.session.isLogin === true){
+		res.sendFile(__dirname + "/html/home.html");	
+	} else {
+		res.write("<script>alert('로그인 안하면 못들어가 바보야')</script>");
+		res.write("<script>window.location=\"/\"</script>");
+		res.end();
+	}
+	
+});
+
+
 
 app.post('/signup_process', (req,res) => {
 	let post = req.body;
@@ -54,6 +81,30 @@ app.post('/signup_process', (req,res) => {
 	res.redirect('/');
 	
 });
+
+app.post('/user_check', (req, res) => {
+	let post = req.body;
+	let email = post.email;
+	let password = crypto.createHash('md5').update(post.password).digest('base64');;
+	
+	db.query(`SELECT * FROM user where email= ? and password = ?`,[email, password],(err, result) => {
+
+		if (result===[] || err) {
+			res.write("<script>alert('등록되지 않은 사용자거나 비밀번호가 틀렸어')</script>");
+			res.write("<script>window.location=\"/\"</script>");
+			res.end();
+		} else {
+			req.session.isLogin = true;
+			req.session.N_NAME = result[0].N_NAME;
+			req.session.save();
+			
+			res.redirect('/home');
+		}
+	});
+	
+});
+
+
 app.listen(80);
 
 
